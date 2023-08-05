db032139a33725514064f11ae27eafffdd0275cd
bigwaveBigwave <gb6ke3@naver.com>
2023-08-05 23:56:36 +0900

    feat : 배열 주소를 이용하여 문자열 뒤집기

--- Reverting_String/Reverting_String.cpp
+++ Reverting_String/Reverting_String.cpp
@@ -7,5 +7,17 @@ int main()
     string s;
     cin >> s;
 
+    int left = 0, right = s.length() - 1;
+
+    while (left < right)
+    {
+        char tmp = s[left];
+        s[left] = s[right];
+        s[right] = tmp;
+        left++;
+        right--;
+    }
+    cout << s;
+
     
 }
